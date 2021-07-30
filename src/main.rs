87dcda44647023947c995b98c975748258575328
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2021-07-30 23:42:53 +0900

    Window surface

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use ash::version::{DeviceV1_0, EntryV1_0, InstanceV1_0};
 use ash::vk;
+use platform::create_surface;
+use std::collections::BTreeSet;
 use std::ffi::CString;
 use std::ptr;
 use validation::{ValidationsRequested, VALIDATION_ON};
@@ -18,17 +20,19 @@ const REQUIRED_VALIDATIONS: [&str; 1] = ["VK_LAYER_KHRONOS_validation"];
 
 struct QueueFamilyIndicesIncomplete {
     graphics_family: Option<usize>,
+    present_family: Option<usize>,
 }
 
 impl QueueFamilyIndicesIncomplete {
     fn is_complete(&self) -> bool {
-        self.graphics_family.is_some()
+        self.graphics_family.is_some() && self.present_family.is_some()
     }
 
     fn get_complete(self) -> Option<QueueFamilyIndices> {
-        if let Some(graphics) = self.graphics_family {
+        if let (Some(graphics), Some(present)) = (self.graphics_family, self.present_family) {
             Some(QueueFamilyIndices {
                 graphics_family: graphics as u32,
+                present_family: present as u32,
             })
         } else {
             None
@@ -38,6 +42,35 @@ impl QueueFamilyIndicesIncomplete {
 
 struct QueueFamilyIndices {
     graphics_family: u32,
+    present_family: u32,
+}
+
+impl QueueFamilyIndices {
+    pub fn as_array(&self) -> [u32; 2] {
+        [self.graphics_family, self.present_family]
+    }
+}
+
+struct Surface {
+    surface: vk::SurfaceKHR,
+    loader: ash::extensions::khr::Surface,
+}
+
+impl Surface {
+    fn new(entry: &ash::Entry, instance: &ash::Instance, window: &Window) -> Surface {
+        let surface =
+            unsafe { create_surface(entry, instance, window) }.expect("Failed to create Surface");
+        let loader = ash::extensions::khr::Surface::new(entry, instance);
+        Surface { surface, loader }
+    }
+
+    fn khr(&self) -> vk::SurfaceKHR {
+        self.surface
+    }
+
+    unsafe fn destroy(&self) {
+        self.loader.destroy_surface(self.surface, None);
+    }
 }
 
 struct VulkanApp {
@@ -45,21 +78,28 @@ struct VulkanApp {
     instance: ash::Instance,
     device: ash::Device,
     graphics_queue: vk::Queue,
+    present_queue: vk::Queue,
+    surface: Surface,
 }
 
 impl VulkanApp {
-    pub fn new() -> VulkanApp {
+    pub fn new(window: &Window) -> VulkanApp {
         let entry = unsafe { ash::Entry::new() }.expect("Could not load Vulkan library");
         let validations = ValidationsRequested::new(&REQUIRED_VALIDATIONS[..]);
         let instance = create_instance(&entry, &validations);
-        let physical_device = pick_physical_device(&instance);
-        let (device, graphics_queue) =
-            create_logical_device(&instance, physical_device, &validations);
+        let surface = Surface::new(&entry, &instance, window);
+        let physical_device = pick_physical_device(&instance, &surface);
+        let (device, queue_indices) =
+            create_logical_device(&instance, physical_device, &validations, &surface);
+        let graphics_queue = unsafe { device.get_device_queue(queue_indices.graphics_family, 0) };
+        let present_queue = unsafe { device.get_device_queue(queue_indices.present_family, 0) };
         VulkanApp {
             entry,
             instance,
             device,
             graphics_queue,
+            present_queue,
+            surface,
         }
     }
 
@@ -71,7 +111,7 @@ impl VulkanApp {
             .expect("Failed to create window")
     }
 
-    pub fn main_loop(event_loop: EventLoop<()>) -> ! {
+    pub fn main_loop(self, event_loop: EventLoop<()>) -> ! {
         event_loop.run(move |event, _, control_flow| match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
@@ -98,6 +138,7 @@ impl VulkanApp {
 impl Drop for VulkanApp {
     fn drop(&mut self) {
         unsafe {
+            self.surface.destroy();
             self.device.destroy_device(None);
             self.instance.destroy_instance(None);
         };
@@ -146,20 +187,29 @@ fn create_instance(entry: &ash::Entry, validations: &ValidationsRequested) -> as
     }
 }
 
-fn pick_physical_device(instance: &ash::Instance) -> vk::PhysicalDevice {
+fn pick_physical_device(instance: &ash::Instance, surface: &Surface) -> vk::PhysicalDevice {
     let physical_devices =
         unsafe { instance.enumerate_physical_devices() }.expect("No physical devices found");
     log::info!("Found {} GPUs in the system", physical_devices.len());
     physical_devices
         .into_iter()
-        .map(|device| (device, rate_physical_device_suitability(instance, device)))
+        .map(|device| {
+            (
+                device,
+                rate_physical_device_suitability(instance, device, surface),
+            )
+        })
         .filter(|(_, score)| *score != 0)
         .max_by_key(|(_, score)| *score)
         .expect("No compatible physical devices found")
         .0
 }
 
-fn rate_physical_device_suitability(instance: &ash::Instance, device: vk::PhysicalDevice) -> u32 {
+fn rate_physical_device_suitability(
+    instance: &ash::Instance,
+    device: vk::PhysicalDevice,
+    surface: &Surface,
+) -> u32 {
     let device_properties = unsafe { instance.get_physical_device_properties(device) };
     let device_features = unsafe { instance.get_physical_device_features(device) };
     let mut score = match device_properties.device_type {
@@ -169,7 +219,7 @@ fn rate_physical_device_suitability(instance: &ash::Instance, device: vk::Physic
         vk::PhysicalDeviceType::OTHER => 10,
         _ => 10,
     };
-    let queues = find_queue_families(instance, device);
+    let queues = find_queue_families(instance, device, surface);
     if device_features.geometry_shader == 0 || !queues.is_complete() {
         score = 0;
     }
@@ -179,31 +229,51 @@ fn rate_physical_device_suitability(instance: &ash::Instance, device: vk::Physic
 fn find_queue_families(
     instance: &ash::Instance,
     device: vk::PhysicalDevice,
+    surface: &Surface,
 ) -> QueueFamilyIndicesIncomplete {
     let queue_families = unsafe { instance.get_physical_device_queue_family_properties(device) };
     let graphics_family = queue_families
-        .into_iter()
+        .iter()
         .position(|queue| queue.queue_flags.contains(vk::QueueFlags::GRAPHICS));
-    QueueFamilyIndicesIncomplete { graphics_family }
+    let present_family = (0..queue_families.len()).into_iter().find(|x| unsafe {
+        surface
+            .loader
+            .get_physical_device_surface_support(device, *x as u32, surface.khr())
+            .unwrap()
+    });
+    QueueFamilyIndicesIncomplete {
+        graphics_family,
+        present_family,
+    }
 }
 
 fn create_logical_device(
     instance: &ash::Instance,
     physical_device: vk::PhysicalDevice,
     validations: &ValidationsRequested,
-) -> (ash::Device, vk::Queue) {
-    let queue_families = find_queue_families(instance, physical_device)
+    surface: &Surface,
+) -> (ash::Device, QueueFamilyIndices) {
+    let queue_families = find_queue_families(instance, physical_device, surface)
         .get_complete()
         .unwrap();
+    let queue_families_set = queue_families
+        .as_array()
+        .iter()
+        .copied()
+        .collect::<BTreeSet<_>>();
+    let mut queue_create_infos = Vec::with_capacity(queue_families_set.len());
     let queue_priority = 1.0;
-    let queue_create_info = vk::DeviceQueueCreateInfo {
-        s_type: vk::StructureType::DEVICE_QUEUE_CREATE_INFO,
-        p_next: ptr::null(),
-        flags: vk::DeviceQueueCreateFlags::empty(),
-        queue_family_index: queue_families.graphics_family,
-        queue_count: 1,
-        p_queue_priorities: &queue_priority,
-    };
+    for queue_index in queue_families_set {
+        let queue_create_info = vk::DeviceQueueCreateInfo {
+            s_type: vk::StructureType::DEVICE_QUEUE_CREATE_INFO,
+            p_next: ptr::null(),
+            flags: vk::DeviceQueueCreateFlags::empty(),
+            queue_family_index: queue_index,
+            queue_count: 1,
+            p_queue_priorities: &queue_priority,
+        };
+        queue_create_infos.push(queue_create_info);
+    }
 
     let physical_features = vk::PhysicalDeviceFeatures {
         ..Default::default()
@@ -212,8 +282,8 @@ fn create_logical_device(
         s_type: vk::StructureType::DEVICE_CREATE_INFO,
         p_next: ptr::null(),
         flags: vk::DeviceCreateFlags::empty(),
-        queue_create_info_count: 1,
-        p_queue_create_infos: &queue_create_info,
+        queue_create_info_count: queue_create_infos.len() as u32,
+        p_queue_create_infos: queue_create_infos.as_ptr(),
         enabled_layer_count: if VALIDATION_ON {
             validations.layers_ptr().len() as u32
         } else {
@@ -230,13 +300,12 @@ fn create_logical_device(
     };
     let device = unsafe { instance.create_device(physical_device, &device_create_info, None) }
         .expect("Failed to create logical device");
-    let queue = unsafe { device.get_device_queue(queue_families.graphics_family, 0) };
-    (device, queue)
+    (device, queue_families)
 }
 
 fn main() {
     let event_loop = EventLoop::new();
-    VulkanApp::init_window(&event_loop);
-    let app = VulkanApp::new();
-    VulkanApp::main_loop(event_loop);
+    let window = VulkanApp::init_window(&event_loop);
+    let app = VulkanApp::new(&window);
+    app.main_loop(event_loop);
 }
